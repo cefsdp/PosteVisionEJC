e9a807d19ea41e35ae3f8178376315b91777c786
Charles Fousse <cefsdp@gmail.com>
2021-04-29 15:38:29 +0200

    Page Accueil

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @navbar = true
+    if user_signed_in?
+    end
   end
 end
