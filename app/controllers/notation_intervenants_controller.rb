64733447ff9398536a0d1e97452aa00977e03cb7
Charles Fousse <cefsdp@gmail.com>
2021-12-10 22:45:07 +0100

    Added Save to sheets of notation intervenants

--- app/controllers/notation_intervenants_controller.rb
+++ app/controllers/notation_intervenants_controller.rb
@@ -11,6 +11,7 @@ class NotationIntervenantsController < ApplicationController
     @notation_intervenant = NotationIntervenant.new(notation_params)
     @notation_intervenant.intervenant = @intervenant
     if @notation_intervenant.save
+      saving_notation_intervenant
       flash[:success] = "NotationIntervenant successfully created"
       redirect_to @etude
     else
@@ -31,6 +32,7 @@ class NotationIntervenantsController < ApplicationController
     @notation_intervenant = NotationIntervenant.find(params[:id])
     @notation_intervenant.intervenant_id = @intervenant
     if @notation_intervenant.update_attributes(notation_params)
+      saving_notation_intervenant
       flash[:success] = "NotationIntervenant was successfully updated"
       redirect_to @etude
     else
@@ -42,6 +44,7 @@ class NotationIntervenantsController < ApplicationController
   def destroy
     @notation = NotationIntervenant.find(params[:id])
     if @notation.destroy
+      saving_notation_intervenant
       flash[:success] = 'NotationIntervenant was successfully deleted.'
     else
       flash[:error] = 'Something went wrong'
@@ -55,4 +58,27 @@ class NotationIntervenantsController < ApplicationController
     params.require(:notation_intervenant).permit(:action_principale, :deroulement_phase, :avis_etudiant, :qualite_travail,
                                                  :disponibilite, :respect_delais, :recommandation, :commentaire)
   end
+
+  def init_google_session
+    @session = GoogleDrive::Session.from_service_account_key("config/google_config.json")
+  end
+  
+  def saving_notation_intervenant
+    init_google_session
+    @ws = @session.spreadsheet_by_key("1noJZd6kty2Ib0345YhRhgrjDNA0SSXmOhhDbXPsl73M").worksheet_by_gid("1326956952")
+    NotationIntervenant.all.each_with_index do |notation_intervenant, row|
+      row += 2
+      @ws[row, 1] = notation_intervenant.intervenant.adherent.num_ba
+      @ws[row, 2] = notation_intervenant.intervenant.phase.etude.references
+      @ws[row, 3] = notation_intervenant.action_principale
+      @ws[row, 4] = notation_intervenant.deroulement_phase
+      @ws[row, 5] = notation_intervenant.avis_etudiant
+      @ws[row, 6] = notation_intervenant.qualite_travail
+      @ws[row, 7] = notation_intervenant.disponibilite
+      @ws[row, 8] = notation_intervenant.respect_delais
+      @ws[row, 9] = notation_intervenant.recommandation
+      @ws[row, 10] = notation_intervenant.commentaire
+    end
+    @ws.save
+  
 end
